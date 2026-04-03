4793739940c8306d1e16af574d1cd328fe2bc88b
zhaoyifan <993445491@qq.com>
2026-04-03 14:35:22 +0800

    fix: improve fullscreen image preview dialog

--- storyboard-app/src/app/components/ui/image-preview-dialog.tsx
+++ storyboard-app/src/app/components/ui/image-preview-dialog.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogContent } from "./dialog";
+import { Dialog, DialogContent, DialogTitle } from "./dialog";
 
 type ImagePreviewDialogProps = {
   open: boolean;
@@ -15,14 +15,15 @@ export function ImagePreviewDialog({
 }: ImagePreviewDialogProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="w-auto max-w-[92vw] border-gray-800 bg-[#0b0b0b] p-2">
-        <div className="max-h-[88vh] overflow-hidden rounded-lg bg-black">
+      <DialogContent className="h-screen max-h-screen w-screen max-w-none border-0 bg-black/95 p-6 shadow-none sm:rounded-none [&>button]:top-5 [&>button]:right-5 [&>button]:bg-black/40 [&>button]:text-white [&>button]:opacity-100 [&>button]:hover:bg-black/60">
+        <DialogTitle className="sr-only">{alt}</DialogTitle>
+        <div className="flex h-full w-full items-center justify-center overflow-hidden">
           <img
             src={src}
             alt={alt}
             loading="eager"
             decoding="async"
-            className="max-h-[88vh] max-w-[88vw] object-contain"
+            className="max-h-full max-w-full select-none object-contain"
           />
         </div>
       </DialogContent>
