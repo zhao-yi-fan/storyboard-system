7f2ac730648e676ebacb45c6dd9c4889bfafd9d5
zhaoyifan <993445491@qq.com>
2026-04-22 22:18:30 +0800

    fix: mount global toaster

--- storyboard-app/src/app/App.tsx
+++ storyboard-app/src/app/App.tsx
@@ -1,10 +1,12 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <div className="dark">
       <RouterProvider router={router} />
+      <Toaster richColors position="top-right" />
     </div>
   );
 }
--- storyboard-app/src/app/components/ui/sonner.tsx
+++ storyboard-app/src/app/components/ui/sonner.tsx
@@ -10,6 +10,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      toastOptions={{
+        classNames: {
+          toast: "!z-[9999]",
+        },
+      }}
       style={
         {
           "--normal-bg": "var(--popover)",
